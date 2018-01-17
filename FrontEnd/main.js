32007f4ba484bdeeef0a13af6194e0c47f3abc9b
Ruiwen <louiseruiwenli@gmail.com>
2018-01-16 23:01:17 -0800

    main.html rendering

---
+++ FrontEnd/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('#login').click(function(){
+    window.location = "login.html";
+  });
+
+  $('#createaccount').click(function(){
+    window.location = "createaccount.html";
+  });
+
+
+
+});
